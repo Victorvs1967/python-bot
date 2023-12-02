ecb2f7d26d6781fd28e7678dd4789b6464403b47
Victorvs1967 <victorsmirnov@me.com>
2023-12-02 21:36:29 +0200

    add keyboard

--- main.py
+++ main.py
@@ -5,13 +5,23 @@ from config import TOKEN
 bot = telebot.TeleBot(TOKEN)
 users = {}
 
-
 @bot.message_handler(commands=['start'])
 def welcome(message):
     chat_id = message.chat.id
-    bot.send_message(chat_id, 'Hi! Welcome to the responser bot! Enter your name.')
-    users[chat_id] = {}
-    bot.register_next_step_handler(message, save_username)
+    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button_support = telebot.types.KeyboardButton(text='Write to support.')
+    button01 = telebot.types.KeyboardButton(text='Button 01')
+    button02 = telebot.types.KeyboardButton(text='Button 01')
+    button03 = telebot.types.KeyboardButton(text='Button 01')
+    keyboard.add(button_support, button01)
+    keyboard.add(button02, button03)
+    bot.send_message(chat_id, 'Hi! Welcome to the responser bot!', reply_markup=keyboard)
+
+@bot.message_handler(commands=['remove_keyboard'])
+def remove_keyboard(message):
+    chat_id = message.chat.id
+    keyboard = telebot.types.ReplyKeyboardRemove()
+    bot.send_message(chat_id, 'Keyboard remove', reply_markup=keyboard)
 
 def save_username(message):
     chat_id = message.chat.id
@@ -31,7 +41,14 @@ def who_i(message):
     chat_id = message.chat.id
     name = users[chat_id]['name']
     surname = users[chat_id]['surname']
-    bot.send_message(chat_id, f"You'r {name} {surname}")
+    bot.send_message(chat_id, f"You are {name} {surname}")
+
+@bot.message_handler(func=lambda message: message.text == 'Write to support.')
+def write_to_support(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, 'Enter your name.')
+    users[chat_id] = {}
+    bot.register_next_step_handler(message, save_username)
 
 
 if __name__ == '__main__':
